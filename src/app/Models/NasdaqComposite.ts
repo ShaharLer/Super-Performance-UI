d83e779a305083573fc540b83ef4cdb3293078f2
Shahar <shaharman5@gmail.com>
2020-10-28 19:16:30 +0200

    add the logic to fetch the nasdaq composite info

---
+++ src/app/Models/NasdaqComposite.ts
@@ -0,0 +1,8 @@
+export class NasdaqComposite {
+  date: Date;
+  ma3: number;
+  ma3Change: number;
+  ma7: number;
+  ma7Change: number;
+  isBuyMarket: boolean;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    NgbModule,
+    NgxSpinnerModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
